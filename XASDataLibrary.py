ea33c400c4439c31e3e7fd007c4e2b4538324a3a
Matt Newville <newville@cars.uchicago.edu>
2011-02-26 00:43:52 -0600

    several improvements to XASDataLibrary.py

--- XASDataLibrary.py
+++ XASDataLibrary.py
@@ -1,38 +1,53 @@
+#!/usr/bin/env python
+"""
+Principle Python interface to XAS Data Library
+requires SQLAlchemy 0.6 or higher
+
+Main Class:  XASDataLibrary
+
+"""
 import os
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import sessionmaker,  mapper, relationship, backref
-  
-class SimpleTable(object):
+import json
+
+class _BaseTable(object):
+    "generic class to encapsulate SQLAlchemy table"
     def __repr__(self):
         name = self.__class__.__name__
         fields = ['%s' % getattr(self, 'name', 'UNNAMED')]
-        if getattr(self, 'id', None) is not None:
-            fields.append('id=%i' % getattr(self, 'id'))
-
         return "<%s(%s)>" % (name, ', '.join(fields))
 
-class Mode(SimpleTable):
+class Mode(_BaseTable):
+    "collection mode table"
     pass
 
-class Facility(SimpleTable):
+class Facility(_BaseTable):
+    "facility table"
     pass
 
-
-class Beamline(SimpleTable):
+class Beamline(_BaseTable):
+    "beamline table"    
     pass
 
-
-class Monochromator(SimpleTable):
+class Monochromator(_BaseTable):
+    "monochromator table"
     pass
 
-class Edge(SimpleTable):
+class EnergyUnits(_BaseTable):
     def __repr__(self):
         name = self.__class__.__name__
-        fields = ['%s' % getattr(self, 'name', 'X'),
-                  '%s' % getattr(self, 'level', '')]
+        fields = ['%s' % getattr(self, 'units', '')]
         return "<%s(%s)>" % (name, ', '.join(fields))
 
-class Element(SimpleTable):
+class Edge(_BaseTable):
+    def __repr__(self):
+        name = self.__class__.__name__
+        fields = ['%s' % getattr(self, 'name', 'X')]
+        # '%s' % getattr(self, 'level', '')]
+        return "<%s(%s)>" % (name, ', '.join(fields))
+
+class Element(_BaseTable):
     def __repr__(self):
         name = self.__class__.__name__
         fields = ['%s' % getattr(self, 'symbol', 'NU'),
@@ -42,22 +57,22 @@ class Element(SimpleTable):
         return "<%s(%s)>" % (name, ', '.join(fields))
 
 
-class Ligand(SimpleTable):
+class Ligand(_BaseTable):
     pass
 
-class CrystalStructure(SimpleTable):
+class CrystalStructure(_BaseTable):
     pass
 
-class Citation(SimpleTable):
+class Citation(_BaseTable):
     pass
 
-class Monchomator(SimpleTable):
+class Monchomator(_BaseTable):
     pass
 
-class Format(SimpleTable):
+class Format(_BaseTable):
     pass
 
-class Person(SimpleTable):
+class Person(_BaseTable):
     def __repr__(self):
         name = self.__class__.__name__
         fields = ['%s %s' % (getattr(self, 'firstname', ''),
@@ -65,7 +80,7 @@ class Person(SimpleTable):
                   '%s' % getattr(self, 'email', 'NO EMAIL')]
         return "<%s(%s)>" % (name, ', '.join(fields))
 
-class Spectra_Rating(SimpleTable):
+class Spectra_Rating(_BaseTable):
     def __repr__(self):
         name = self.__class__.__name__
         fields = ['%i' % (int(getattr(self, 'score', 0)))]
@@ -74,7 +89,7 @@ class Spectra_Rating(SimpleTable):
             
         return "<%s(%s)>" % (name, ', '.join(fields))
 
-class Suite_Rating(SimpleTable):
+class Suite_Rating(_BaseTable):
     def __repr__(self):
         name = self.__class__.__name__
         fields = ['%i' % (int(getattr(self, 'score', 0)))]
@@ -83,13 +98,13 @@ class Suite_Rating(SimpleTable):
             
         return "<%s(%s)>" % (name, ', '.join(fields))
 
-class Suite(SimpleTable):
+class Suite(_BaseTable):
     pass
 
-class Sample(SimpleTable):
+class Sample(_BaseTable):
     pass
 
-class Spectra(SimpleTable):
+class Spectra(_BaseTable):
     pass
 
 
@@ -105,99 +120,111 @@ class XASDataLibrary(object):
         self.metadata =  MetaData(self.engine)
         
         self.metadata.reflect()
-        class Empty:
-            pass
-        self.tables = Empty()
-        for tablename in self.metadata.tables:
-            setattr(self.tables, tablename, self.metadata.tables[tablename])
-            
+        tables = self.tables = self.metadata.tables
+
         self.session = sessionmaker(bind=self.engine)()
 
-        mapper(Mode,     self.tables.mode, 
-               properties={'spectra': relationship(Spectra, backref='mode',
-                                                   secondary=self.tables.spectra_mode)}
-        )
+        mapper(Mode,     tables['mode'], 
+               properties={'spectra':
+                           relationship(Spectra, backref='mode',
+                                        secondary=tables['spectra_mode'])})
 
-        mapper(Edge,     self.tables.edge,
-               properties={'spectra': relationship(Spectra, backref='edge')}
-               )
+        mapper(Edge, tables['edge'],
+               properties={'spectra':
+                           relationship(Spectra, backref='edge')})
                 
-        mapper(Element,  self.tables.element,
-               properties={'spectra': relationship(Spectra, backref='element')}
-               )
+        mapper(Element, tables['element'],
+               properties={'spectra':
+                           relationship(Spectra, backref='element')})
                
-        mapper(Format,   self.tables.format,
-               properties={'spectra': relationship(Spectra, backref='format')}
-               )
+        mapper(Format, tables['format'],
+               properties={'spectra':
+                           relationship(Spectra, backref='format')})
 
-        mapper(Beamline, self.tables.beamline,
-               properties={'spectra': relationship(Spectra, backref='beamline')}
-               )               
+        mapper(Beamline, tables['beamline'],
+               properties={'spectra':
+                           relationship(Spectra, backref='beamline')})
 
-        mapper(Citation, self.tables.citation,
-               properties={'spectra': relationship(Spectra, backref='citation')}
-               )
+        mapper(Citation, tables['citation'],
+               properties={'spectra':
+                           relationship(Spectra, backref='citation')})
                
-        mapper(Ligand,   self.tables.ligand,
-               properties={'spectra': relationship(Spectra, backref='ligand',
-                                                   secondary=self.tables.spectra_ligand)}
-               )               
+        mapper(Ligand,   tables['ligand'],
+               properties={'spectra':
+                           relationship(Spectra, backref='ligand',
+                                        secondary=tables['spectra_ligand'])})
 
-        mapper(CrystalStructure,   self.tables.crystal_structure,
-               properties={'samples': relationship(Sample, backref='structure'),
-                           }
-               )
+        mapper(CrystalStructure,   tables['crystal_structure'],
+               properties={'samples':
+                           relationship(Sample, backref='structure')})
 
-        mapper(Monochromator,   self.tables.monochromator,
+        mapper(Monochromator,   tables['monochromator'],
                properties={'beamlines':
                            relationship(Beamline, backref='monochromator'),
-                           'spectra': relationship(Spectra, backref='monochromator')
-                           }
-               )
+                           'spectra':
+                           relationship(Spectra, backref='monochromator')})
 
-        mapper(Facility, self.tables.facility,
+        mapper(Facility, tables['facility'],
                properties={'beamlines':
                            relationship(Beamline, backref='facility')})
-               
 
-        mapper(Person,   self.tables.person,
-               properties={'suites': relationship(Suite, backref='person'),
-                           'samples': relationship(Sample, backref='person'),
-                           'spectra': relationship(Spectra, backref='person')})
-               
+        mapper(Person,   tables['person'],
+               properties={'suites':
+                           relationship(Suite, backref='person'),
+                           'samples':
+                           relationship(Sample, backref='person'),
+                           'spectra':
+                           relationship(Spectra, backref='person')})
 
-        mapper(Spectra_Rating,   self.tables.spectra_rating)
+        mapper(EnergyUnits,   tables['energy_units'],
+               properties={'monochromator':
+                           relationship(Monochromator, backref='energy_units'),
+                           'spectra':
+                           relationship(Spectra, backref='energy_units')})
 
-        mapper(Suite_Rating,   self.tables.suite_rating)
+        mapper(Spectra_Rating,   tables['spectra_rating'])
 
+        mapper(Suite_Rating,   tables['suite_rating'])
 
-        mapper(Suite,   self.tables.suite,
-               properties={'spectra': relationship(Spectra, backref='suite',
-                                                   secondary=self.tables.spectra_suite)}
-               )
-        
-        mapper(Sample,  self.tables.sample)
+        mapper(Suite,   tables['suite'],
+               properties={'spectra':
+                           relationship(Spectra, backref='suite',
+                                        secondary=tables['spectra_suite'])})
 
-        mapper(Spectra, self.tables.spectra)        
+        mapper(Sample,  tables['sample'])
+
+        mapper(Spectra, tables['spectra'])        
 
         
     def commit(self):
+        "commit session state"
         return self.session.commit()
         
     def query(self, *args, **kws):
+        "generic query"
         return self.session.query(*args, **kws)
 
-    def add_facility(self, name, notes=None, attributes=None, **kws):
-        """add facilty by name, return Facility instance"""
-        fac = Facility() #
-        fac.name = name
-        if notes is not None:
-            fac.notes = notes
-        if attributes is not None:
-            fac.attributes = attributes
-        self.session.add(fac)
+    def __addRow(self, table, argnames, argvals, **kws):
+        """add generic row"""
+        me = table() #
+        arg0 = argnames[0]
+        val0 = argvals[0]
+        for name, val in zip(argnames, argvals):
+            setattr(me, name, val)
+        for key, val in kws.items():
+            if key == 'attributes':
+                if not isinstance(val, (str, unicode)):
+                    val = json.dumps(val)
+            setattr(me, key, val)
+        self.session.add(me)
         self.session.commit()
-        return self.query(Facility).filter(Facility.name==name).one()
+        return self.query(table).filter(getattr(table, arg0)==val0).one()
+
+    def add_facility(self, name, notes=None, attributes='', **kws):
+        """add facilty by name, return Facility instance"""
+        kws['notes'] = notes
+        kws['attributes'] = attributes
+        return self.__addRow(Facility, ('name',), (name,), **kws)
 
     def add_beamline(self, name, facility=None, notes=None,
                      xray_source=None, monochromator=None,
@@ -206,40 +233,55 @@ class XASDataLibrary(object):
                facility= Facility instance or id
                monochromator= Monochromator or id
                returns Beamline instance"""
-        bl = Beamline() #
-        bl.name = name
         if isinstance(facility, Facility):
             facility = facility.id
-        if facility is not None:
-            bl.facility_id = facility
-
         if isinstance(monochromator, Monochromator):
             monochromator = monochromator.id
-        if monochromator is not None:
-            bl.monochromator_id = monochromator
 
-        bl.notes = notes
-        bl.xray_source = xray_source
-        bl.attributes = attributes
-        
-        self.session.add(bl)
-        self.session.commit()
-        return self.query(Beamline).filter(Beamline.name==name).one()
+        kws['notes'] = notes
+        kws['attributes'] = attributes
+        kws['xray_source'] = xray_source
+        kw['facility'] = facility            
+        kw['monochromator'] = monochromator
 
-       
+        return self.__addRow(Beamline, ('name',), (name,), **kws)
 
-def add_beamline(name, notes, facility_id):
-    print 'add beamline! ', name, notes, facility_id
+    def add_person(self, firstname, lastname, email,
+                   affiliation=None, attributes=None, **kws):
+        """add person: arguments are
+        firstname, lastname, email  with
+        affiliation and attributes optional
+        returns Person instance"""
+        kws['affiliation'] = affiliation
+        kws['attributes'] = attributes
+        return self.__addRow(Person, ('email','firstname','lastname'),
+                             (email, firstname, lastname), **kws)
 
-def add_person(firstname, lastname, email):
-    print 'add person! ', name, notes
+    def add_ligand(self, name, notes=None, attributes=None, **kws):
+        """add ligand: name required
+        notes and attributes optional
+        returns Ligand instance"""
+        kws['notes'] = notes
+        kws['attributes'] = attributes
+
+        return self.__addRow(Ligand, ('name',), (name,), **kws)        
+         
+    def add_energy_units(self, units, notes=None, attributes=None, **kws):
+        """add Energy Units: units required
+        notes and attributes optional
+        returns EnergyUnits instance"""
+        kws['notes'] = notes
+        kws['attributes'] = attributes
+
+        return self.__addRow(EnergyUnits, ('units',), (units,), **kws)        
 
 if __name__ == '__main__':    
     xaslib =  XASDataLibrary('xasdat.sqlite')
 
-    print dir(xaslib.tables)
+    print xaslib.tables.keys()
+    print 'Edges:'
     for f in xaslib.query(Edge):
-        print f
+        print ' -- ', f, f.level
     
     print 'Facilities:'
     for f in xaslib.query(Facility):
@@ -248,22 +290,6 @@ if __name__ == '__main__':
     print 'Beamlines:'
     for f in xaslib.query(Beamline):
         print ' -- ', f,  f.notes, f.facility
-# 
-#     print '---'
-#     x13id = Beamline() #
-#     print dir(x13id)
-#     
-#     x13id.name = '13IDC' # , 
-#     x13id.notes = 'GSECARS ID-C'
-#     x13id.xray_source = 'APS UA'
-#     x13id.facility = 1
-#  
-#     xaslib.session.add(x13id)
-#  
-#     xaslib.session.commit()
-#     for f in xaslib.session.query(Beamline):
-#         print f.name, f.notes, f.facility_ref
-# 
-#     for f in xaslib.session.query(Facility):
-#         print f, f.beamlines
-# 
+    print 'EnergyUnits:'
+    for f in xaslib.query(EnergyUnits):
+        print ' -- ', f, f.notes
