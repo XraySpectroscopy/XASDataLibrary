388dedcd751246ded1345d5a6a68a06b7c5a1a66
Matt Newville <newville@cars.uchicago.edu>
2011-02-26 00:43:43 -0600

    several improvements to XASDataLibrary.py

--- XASDataLibrary.py
+++ XASDataLibrary.py
@@ -1,67 +1,269 @@
 import os
-import sys
-import time
-import sqlite3
+from sqlalchemy import MetaData, create_engine
+from sqlalchemy.orm import sessionmaker,  mapper, relationship, backref
+  
+class SimpleTable(object):
+    def __repr__(self):
+        name = self.__class__.__name__
+        fields = ['%s' % getattr(self, 'name', 'UNNAMED')]
+        if getattr(self, 'id', None) is not None:
+            fields.append('id=%i' % getattr(self, 'id'))
 
-SCHEMA = open('Create_XASDataLib.sql', 'r').read()
+        return "<%s(%s)>" % (name, ', '.join(fields))
 
-TABLES = ('spectra', 'sample', 'crystal', 'person', 'citation', 'rating',
-          'suite', 'facility', 'beamline', 'monochomator',
-          'collection_mode', 'ligand', 'format', 'element', 'edge',
-          'spectra_modes', 'spectra_ligand', 'spectra_suite' )
+class Mode(SimpleTable):
+    pass
 
-class BaseTable:
-    def __init__(self, table=None, dbcursor=None):
-        self.table = table
-        self.dbcursor = dbcursor
+class Facility(SimpleTable):
+    pass
 
-    def sqlexec(self, query, vals=None):
-        if vals is None:
-            vals = ()
-            self.dbcursor.execute(query, vals)
-        return self.cursor.fetchall()
 
-    def select(self, where=None):
-        if where is None:
-            where = '1=1'
-        return self.sqlexec("select from %s where %s" % (self.table, where))
-        
-def element(BaseTable):
-    def __init__(self, dbcursor=None):
-        BaseTable.__init__(table='element', dbcursor=dbcursor)
+class Beamline(SimpleTable):
+    pass
 
-    def get_z(self, symbol=None, name=None):
-        # if symbol is None
-        query = 'select from '
-        
+
+class Monochromator(SimpleTable):
+    pass
+
+class Edge(SimpleTable):
+    def __repr__(self):
+        name = self.__class__.__name__
+        fields = ['%s' % getattr(self, 'name', 'X'),
+                  '%s' % getattr(self, 'level', '')]
+        return "<%s(%s)>" % (name, ', '.join(fields))
+
+class Element(SimpleTable):
+    def __repr__(self):
+        name = self.__class__.__name__
+        fields = ['%s' % getattr(self, 'symbol', 'NU'),
+                  'Z=%i' % getattr(self, 'z', 0),
+                  '%s' % getattr(self, 'name', 'UNNAMED'),
+                  ]
+        return "<%s(%s)>" % (name, ', '.join(fields))
+
+
+class Ligand(SimpleTable):
+    pass
+
+class CrystalStructure(SimpleTable):
+    pass
+
+class Citation(SimpleTable):
+    pass
+
+class Monchomator(SimpleTable):
+    pass
+
+class Format(SimpleTable):
+    pass
+
+class Person(SimpleTable):
+    def __repr__(self):
+        name = self.__class__.__name__
+        fields = ['%s %s' % (getattr(self, 'firstname', ''),
+                             getattr(self, 'lastname', '')),
+                  '%s' % getattr(self, 'email', 'NO EMAIL')]
+        return "<%s(%s)>" % (name, ', '.join(fields))
+
+class Spectra_Rating(SimpleTable):
+    def __repr__(self):
+        name = self.__class__.__name__
+        fields = ['%i' % (int(getattr(self, 'score', 0)))]
+        if getattr(self, 'spectra', None) is not None:
+            fields.append('Spectra %i' % getattr(self, 'spectra', 0))
+            
+        return "<%s(%s)>" % (name, ', '.join(fields))
+
+class Suite_Rating(SimpleTable):
+    def __repr__(self):
+        name = self.__class__.__name__
+        fields = ['%i' % (int(getattr(self, 'score', 0)))]
+        if getattr(self, 'suite', None) is not None:
+            fields.append('Suite %i' % getattr(self, 'suite', 0))
+            
+        return "<%s(%s)>" % (name, ', '.join(fields))
+
+class Suite(SimpleTable):
+    pass
+
+class Sample(SimpleTable):
+    pass
+
+class Spectra(SimpleTable):
+    pass
+
+
 class XASDataLibrary(object):
-    def __init__(self,dbname='xaslib.sqlite'):
+    def __init__(self, dbname='xaslib.sqlite'):
+
+        if not os.path.exists(dbname):
+            raise IOError("Database '%s' not found!" % dbname)
+        
+        
         self.dbname = dbname
-        self.conn   = sqlite3.connect(self.dbname,
-                                      isolation_level=None)
-        self.cursor = self.conn.cursor()
-        self.__sql_statements = []
-        if not self.validate_db():
-            self._create()
+        self.engine  = create_engine('sqlite:///%s' % self.dbname)
+        self.metadata =  MetaData(self.engine)
+        
+        self.metadata.reflect()
+        class Empty:
+            pass
+        self.tables = Empty()
+        for tablename in self.metadata.tables:
+            setattr(self.tables, tablename, self.metadata.tables[tablename])
+            
+        self.session = sessionmaker(bind=self.engine)()
 
-    def _create(self):
-        print 'Creating new empty database'
-        return self.cursor.executescript(SCHEMA)
+        mapper(Mode,     self.tables.mode, 
+               properties={'spectra': relationship(Spectra, backref='mode',
+                                                   secondary=self.tables.spectra_mode)}
+        )
 
-    def sql_exec(self, sql, vals=None):
-        if vals is None:
-            vals = ()
-        self.cursor.execute(sql, vals)
-        return self.cursor.fetchall()
+        mapper(Edge,     self.tables.edge,
+               properties={'spectra': relationship(Spectra, backref='edge')}
+               )
+                
+        mapper(Element,  self.tables.element,
+               properties={'spectra': relationship(Spectra, backref='element')}
+               )
+               
+        mapper(Format,   self.tables.format,
+               properties={'spectra': relationship(Spectra, backref='format')}
+               )
 
-    def validate_db(self): rows = self.sql_exec("""select tbl_name from
-        sqlite_master where type='table' order by tbl_name""") tables =
-        [row[0] for row in rows] print 'Validate ',rows, tables for
-        req_table in TABLES: if req_table not in tables: return False
-        return True
+        mapper(Beamline, self.tables.beamline,
+               properties={'spectra': relationship(Spectra, backref='beamline')}
+               )               
 
-if __name__ == '__main__':
-    datlib = XASDataLibrary(dbname='test.sqlite')
-    print 'Hello'
+        mapper(Citation, self.tables.citation,
+               properties={'spectra': relationship(Spectra, backref='citation')}
+               )
+               
+        mapper(Ligand,   self.tables.ligand,
+               properties={'spectra': relationship(Spectra, backref='ligand',
+                                                   secondary=self.tables.spectra_ligand)}
+               )               
+
+        mapper(CrystalStructure,   self.tables.crystal_structure,
+               properties={'samples': relationship(Sample, backref='structure'),
+                           }
+               )
+
+        mapper(Monochromator,   self.tables.monochromator,
+               properties={'beamlines':
+                           relationship(Beamline, backref='monochromator'),
+                           'spectra': relationship(Spectra, backref='monochromator')
+                           }
+               )
+
+        mapper(Facility, self.tables.facility,
+               properties={'beamlines':
+                           relationship(Beamline, backref='facility')})
+               
+
+        mapper(Person,   self.tables.person,
+               properties={'suites': relationship(Suite, backref='person'),
+                           'samples': relationship(Sample, backref='person'),
+                           'spectra': relationship(Spectra, backref='person')})
+               
+
+        mapper(Spectra_Rating,   self.tables.spectra_rating)
+
+        mapper(Suite_Rating,   self.tables.suite_rating)
+
+
+        mapper(Suite,   self.tables.suite,
+               properties={'spectra': relationship(Spectra, backref='suite',
+                                                   secondary=self.tables.spectra_suite)}
+               )
+        
+        mapper(Sample,  self.tables.sample)
+
+        mapper(Spectra, self.tables.spectra)        
+
+        
+    def commit(self):
+        return self.session.commit()
+        
+    def query(self, *args, **kws):
+        return self.session.query(*args, **kws)
+
+    def add_facility(self, name, notes=None, attributes=None, **kws):
+        """add facilty by name, return Facility instance"""
+        fac = Facility() #
+        fac.name = name
+        if notes is not None:
+            fac.notes = notes
+        if attributes is not None:
+            fac.attributes = attributes
+        self.session.add(fac)
+        self.session.commit()
+        return self.query(Facility).filter(Facility.name==name).one()
+
+    def add_beamline(self, name, facility=None, notes=None,
+                     xray_source=None, monochromator=None,
+                     attributes=None, **kws):
+        """add beamline by name, with facility:
+               facility= Facility instance or id
+               monochromator= Monochromator or id
+               returns Beamline instance"""
+        bl = Beamline() #
+        bl.name = name
+        if isinstance(facility, Facility):
+            facility = facility.id
+        if facility is not None:
+            bl.facility_id = facility
+
+        if isinstance(monochromator, Monochromator):
+            monochromator = monochromator.id
+        if monochromator is not None:
+            bl.monochromator_id = monochromator
+
+        bl.notes = notes
+        bl.xray_source = xray_source
+        bl.attributes = attributes
+        
+        self.session.add(bl)
+        self.session.commit()
+        return self.query(Beamline).filter(Beamline.name==name).one()
+
+       
+
+def add_beamline(name, notes, facility_id):
+    print 'add beamline! ', name, notes, facility_id
+
+def add_person(firstname, lastname, email):
+    print 'add person! ', name, notes
+
+if __name__ == '__main__':    
+    xaslib =  XASDataLibrary('xasdat.sqlite')
+
+    print dir(xaslib.tables)
+    for f in xaslib.query(Edge):
+        print f
     
-    
+    print 'Facilities:'
+    for f in xaslib.query(Facility):
+        print  ' -- ', f, f.notes, f.beamlines
+
+    print 'Beamlines:'
+    for f in xaslib.query(Beamline):
+        print ' -- ', f,  f.notes, f.facility
+# 
+#     print '---'
+#     x13id = Beamline() #
+#     print dir(x13id)
+#     
+#     x13id.name = '13IDC' # , 
+#     x13id.notes = 'GSECARS ID-C'
+#     x13id.xray_source = 'APS UA'
+#     x13id.facility = 1
+#  
+#     xaslib.session.add(x13id)
+#  
+#     xaslib.session.commit()
+#     for f in xaslib.session.query(Beamline):
+#         print f.name, f.notes, f.facility_ref
+# 
+#     for f in xaslib.session.query(Facility):
+#         print f, f.beamlines
+# 
