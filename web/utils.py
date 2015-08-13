d2082df6b5899104566a8f3265f26c5e9efcab89
Matt Newville <newville@cars.uchicago.edu>
2015-08-13 16:27:09 -0500

    add lists for most session items, many fixes

--- web/utils.py
+++ web/utils.py
@@ -66,19 +66,26 @@ def session_init(session, db):
 
     if 'elements' not in session:
         session['elements'] = d = {}
+        session['element_list'] = l = []        
         for r in db.get_elements():
             d['%i'%r.z]  = (r.symbol, r.name)
-
+            l.append({'z': '%i' % r.z, 'symbol': r.symbol,
+                      'name': r.name})
+            
     if 'edges' not in session:
         session['edges'] = d = {}
+        session['edge_list'] = l = []
         for r in db.get_edges():
             d['%i'%r.id]  = r.name
+            l.append({'id': '%i' % r.id, 'name': r.name})
 
     if 'energy_units' not in session:
         session['energy_units'] = d = {}
+        session['energy_units_list'] = l = []
         for r in db.filtered_query('energy_units'):
             d['%i'%r.id] = r.units
-
+            l.append({'id': '%i' % r.id, 'units': r.units})
+            
     if 'facilities' not in session:
         session['facilities'] = d = {}
         for r in db.get_facility():
@@ -87,9 +94,19 @@ def session_init(session, db):
 
     if 'beamlines' not in session:
         session['beamlines'] = d = {}
+        session['beamline_list'] = l = []
         for r in db.get_beamlines():
             facid = '%i' % r.facility_id
-            d['%i'%r.id] = (r.name, r.notes, r.xray_source, facid)
+            facdat = session['facilities'][facid]
+            d['%i'%r.id] = (r.name, r.notes, r.xray_source, facid, facdat[0])
+            l.append({'id': '%i' % r.id,
+                      'name': r.name,
+                      'notes': r.notes,
+                      'xray_source': r.xray_source,
+                      'fac_id': facid,
+                      'fac_name': facdat[0],
+                      'fac_loc': "%s, %s" % (facdat[3], facdat[5])})
+
 
     if 'spectra' not in session:
         session['spectra'] = d = {}
@@ -98,26 +115,38 @@ def session_init(session, db):
 
     if 'samples' not in session:
         session['samples'] = d = {}
+        session['sample_list'] = l = []           
         for r in db.filtered_query('sample'):
             d['%i'%r.id] = (r.name, r.formula, r.preparation,
                             r.material_source, r.notes, '%i'%r.person_id)
-
+            l.append({'id': '%i' % r.id, 'name': r.name,
+                      'formula': r.formula, 'notes': r.notes,
+                      'preparation': r.preparation, 
+                      'person_id': r.person_id,
+                      'material_source': r.material_source})
+            
     if 'suites' not in session:
         session['suites'] = d = {}
+        session['suite_list'] = l = []
         for r in db.filtered_query('suite'):
             d['%i'%r.id] = (r.name, r.notes, '%i'%r.person_id)
-
+            l.append({'id': '%i' % r.id, 'name': r.name,
+                       'notes': r.notes, 'person_id': r.person_id})
+            
     if 'people' not in session:
         session['people'] = d = {}
+        session['people_list'] = l = []
         for r in db.get_persons():
             d['%i'%r.id] = (r.email, r.name, r.affiliation)
+            l.append({'id': '%i' % r.id, 'email': r.email, 'name': r.name,
+                      'affiliation': r.affiliation})
 
+            
 def spectrum_ratings(db, sid):
     """list of score, comments, time, person) for spectrum ratings"""
     d = []
     for r in db.filtered_query('spectrum_rating', spectrum_id=sid):
         d.append((r.score, r.comments, r.datetime, "%i" % r.person_id))
-
     return d
 
 def suite_ratings(db, sid):
@@ -146,11 +175,11 @@ def spectra_for_beamline(db, session, blid):
     for r in db.get_spectra():
         if r.beamline_id is None or int(r.beamline_id) !=int(blid):
             continue
-        _nam, _z, _edge, _pid = session['spectra']["%i" % (r.spectrum_id)]
+        _nam, _z, _edge, _pid = session['spectra']["%i" % (r.id)]
         elem_sym, elem_name = session['elements']["%i" % _z]
         edge =  session['edges']["%i" % _edge]
-        spectra.append({'spectrum_id': r.spectrum_id,
-                        'name':    r.spectrum_name,
+        spectra.append({'spectrum_id': r.id,
+                        'name':    r.name,
                         'elem_sym': elem_sym, 'edge': edge})
     return spectra
 
@@ -171,24 +200,30 @@ def parse_spectrum(s, session):
     person = session['people']['%i' % s.person_id]
 
     eunits = session['energy_units']['%i'% s.energy_units_id]
-    dspace = '%f'% s.d_spacing
+    d_spacing = '%f'% s.d_spacing
 
     notes =  json.loads(s.notes)
 
     try:
-        beamline = session['beamlines']['%i'% s.beamline_id]
+        beamline_id  = '%i'% s.beamline_id
+        beamline     = session['beamlines'][beamline_id]
+        beamline_desc = '%s @ %s ' % (beamline[0], beamline[4])
     except:
-        beamline = 'unknown (check unsorted headers)'
+        beamline_desc = 'unknown '
+        beamline = None
+        beamline_id  = '-1'
         tname = notes.get('beamline', {}).get('name', None)
         if tname is not None:
-            beamline = "%s may be '%s'" % (beamline, tname)
+            beamline_desc = "%s -- may be '%s'" % (beamline_desc, tname)
 
     try:
         sample = session['samples']['%i'% s.sample_id]
+        sample_id = '%i'% s.sample_id
         sample_name = sample[0]
         sample_form = sample[1]
         sample_prep = sample[2]
     except:
+        sample_id  = '-1'        
         sample_name = 'unknown'
         sample_form = 'unknown'
         sample_prep = 'unknown'
@@ -219,12 +254,15 @@ def parse_spectrum(s, session):
             'elem_name': elem[1],
             'edge': edge,
             'energy_units': eunits,
+            'raw_comments': s.comments,
             'comments': multiline_text(s.comments),
-            'beamline_id': s.beamline_id,
+            'beamline_id': beamline_id,
+            'beamline_desc': beamline_desc,
             'beamline': beamline,
             'mononame': mononame,
-            'dspace': dspace,
+            'd_spacing': d_spacing,
             'misc': misc,
+            'sample_id':   sample_id,
             'sample_name':  sample_name,
             'sample_form':  sample_form,
             'sample_prep':  sample_prep,
