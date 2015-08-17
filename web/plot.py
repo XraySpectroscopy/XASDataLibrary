28ca71e8866564ade354d49512d73cf959edd171
Matt Newville <newville@cars.uchicago.edu>
2015-08-17 13:58:26 -0500

    add reference sample to XANES plot if available

--- web/plot.py
+++ web/plot.py
@@ -5,25 +5,32 @@ import base64
 import numpy as np
 import matplotlib
 matplotlib.use('agg')
-import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import Figure, FigureCanvas
+
 from matplotlib.font_manager import FontProperties
 from matplotlib import rcParams
 
 mpl_lfont = FontProperties()
-mpl_lfont.set_size(9)
-rcParams['xtick.labelsize'] =  rcParams['ytick.labelsize'] = 9
+mpl_lfont.set_size(18)
+rcParams['xtick.labelsize'] =  rcParams['ytick.labelsize'] = 18
 
-def make_xafs_plot(x, y, title, xlabel='Energy (eV)', ylabel='mu', x0=None):
-    fig  = plt.figure(figsize=(4.25, 2.5), dpi=100)
+def make_xafs_plot(x, y, title, xlabel='Energy (eV)', ylabel='mu', x0=None,
+                   ref_mu=None, ref_name=None):
+
+    fig  = Figure(figsize=(8.5, 5.0), dpi=300)
+    canvas = FigureCanvas(fig)
     axes = fig.add_axes([0.16, 0.16, 0.75, 0.75], axisbg='#FFFFFF')
 
     axes.set_xlabel(xlabel, fontproperties=mpl_lfont)
     axes.set_ylabel(ylabel, fontproperties=mpl_lfont)
-    axes.set_title(title, fontproperties=mpl_lfont)
-    axes.plot(x, y, linewidth=2)
+    axes.plot(x, y, linewidth=4.5)
     if x0 is not None:
         axes.axvline(0, ymin=min(y), ymax=max(y),
-                     linewidth=1, color='#CCBBDD', zorder=-20)
+                     linewidth=2, color='#CCBBDD', zorder=-10)
+
+        if ref_mu is not None:
+            axes.plot(x, ref_mu, linewidth=4, zorder=-5, color='#AA4444')
+            title = "%s (%s)" % (title, ref_name)
         # ymax = (0.7*max(y) + 0.3 * min(y))
         # axes.text(-25.0, ymax, "%.1f eV" % x0, fontproperties=mpl_lfont)
 
@@ -32,8 +39,9 @@ def make_xafs_plot(x, y, title, xlabel='Energy (eV)', ylabel='mu', x0=None):
 
     axes.set_xlim((min(x)-xrange*0.05, max(x)+xrange*0.05), emit=True)
     axes.set_ylim((min(y)-yrange*0.05, max(y)+yrange*0.05), emit=True)
+    axes.set_title(title, fontproperties=mpl_lfont)
 
     figdata = io.BytesIO()
-    plt.savefig(figdata, format='png', facecolor='#FDFDFA')
+    fig.savefig(figdata, format='png', facecolor='#FDFDFA')
     figdata.seek(0)
     return base64.b64encode(figdata.getvalue())
