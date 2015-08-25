aa4b8babc6422b30adc8ecafde3b7b1d7faa967e
Matt Newville <newville@cars.uchicago.edu>
2015-08-25 09:12:36 +0200

    add simpler script to create empty db

---
+++ create_empty_db.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# creates an empty, ready-to-be-used XAS Spectral DB
+#
+
+from __future__ import print_function
+import sys
+import os
+import glob
+import xasdb
+
+dbname = 'example.db'
+
+if len(sys.argv) > 1:
+    dbname = sys.argv[1]
+
+if os.path.exists(dbname):
+    print("Error:  file '%s' exists" % dbname)
+    print("Usage:  create_empty_db.py  filename")
+
+else:
+    xasdb.create_xasdb(dbname)
+    print( 'Created database %s' % dbname)
+
+    db = xasdb.connect_xasdb(dbname)
+    print( 'Verified that database %s can be accessed.' % dbname)
