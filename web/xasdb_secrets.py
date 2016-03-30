0e0bc90d83600f8f5620decfa1ea0bcc86e07a98
Matthew Newville <newville@cars.uchicago.edu>
2016-03-30 07:34:07 -0500

    add default ADMIN_EMAIL

--- web/xasdb_secrets.py
+++ web/xasdb_secrets.py
@@ -4,10 +4,11 @@ SECRET_KEY = 'some long random string goes here'
 DBNAME = 'xaslib.db'
 
 DBCONN = dict(user='xasdb', password='secret',
-              host='localhost', server='sqlite')
+              host='localhost', server='sqlite3')
 
 PORT = 7112
 DEBUG = True
 LOCAL_ONLY = False
 
 UPLOAD_FOLDER='/tmp'
+ADMIN_EMAIL='nobody@here'
