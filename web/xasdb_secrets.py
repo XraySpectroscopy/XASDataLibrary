a5d402b0be3b0b5514842aec82bb33a19a64eacd
Matthew Newville <newville@cars.uchicago.edu>
2020-04-05 12:17:45 -0500

    put more constants into xasdb_secrets config file

--- web/xasdb_secrets.py
+++ web/xasdb_secrets.py
@@ -3,11 +3,14 @@ SECRET_KEY = 'some long random string goes here'
 
 DBNAME = 'xaslib.db'
 
-DBCONN = dict(user='xasdb', password='secret',
-              host='localhost', server='sqlite3')
+DBCONN = dict(user='xasdb',
+              password='secret',
+              host='localhost',
+              server='sqlite3')
 
 PORT = 7112
 DEBUG = True
-LOCAL_ONLY = False
-UPLOAD_FOLDER='/tmp'
-ADMIN_EMAIL='xaslib@xrayabsorption.org'
+UPLOAD_FOLDER = '/tmp'
+ADMIN_EMAIL = 'xaslib@xrayabsorption.org'
+BASE_URL    = 'https://data.xrayabsorption.org'
+LOCAL_ONLY  = False
