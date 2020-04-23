be8fa0e4bc90596c03623b666e47cf6bb34bf840
Matthew Newville <newville@cars.uchicago.edu>
2020-04-22 22:58:22 -0500

    debug by deault

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from xaslib import app
 # Note: Change This Key, used for Flask cookies
 # SECRET_KEY = 'xaslib_'+base64.b64encode(os.urandom(32))[:16]
 SECRET_KEY = 'xaslib_secret_key_0hrZzetj5rDDSQXv'
+DEBUG = True
 
 PORT = 7112
 cnf = app.config
@@ -20,7 +21,7 @@ cnf['UPLOAD_FOLDER'] = '/tmp/xaslib_uploads'
 cnf['MAX_CONTENT_LENGTH'] = 33554432
 
 app.jinja_env.cache = {}
-
+app.debug  = True
 app.secret_key = SECRET_KEY
 
 app.run(port=PORT)
