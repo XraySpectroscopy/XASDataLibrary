ec1f3fef6d6a670dbfb8eed2a5ef0c4c423261ac
Matthew Newville <newville@cars.uchicago.edu>
2020-05-13 14:32:23 -0500

    many doc styling changes to match xaslib

--- doc/conf.py
+++ doc/conf.py
@@ -8,12 +8,13 @@ project = 'X-ray Absorption Data Library'
 copyright = 'Public Domain.'
 release = '0.1'
 html_title  = 'X-ray Absorption Data Library'
-# html_short_title = 'data.xrayabsorption.org'
+html_short_title = 'xaslib.xrayabsorption.org'
 
 pygments_style = 'sphinx'
-html_theme = 'nature'
+html_theme = 'classic'
 
-extensions = ['sphinx.ext.todo',   'sphinx.ext.mathjax']
+extensions = ['sphinx.ext.todo',
+              'sphinx.ext.mathjax']
 
 templates_path = ['_templates']
 source_suffix = '.rst'
@@ -28,6 +29,40 @@ add_module_names = False
 
 html_static_path = ['_static']
 html_favicon = '_static/ixas_logo.ico'
-html_sidebars = {'index': ['globaltoc.html','searchbox.html']}
+# html_sidebars = {'index': ['globaltoc.html','searchbox.html']}
 
 language = None
+html_theme_options = {
+    "rightsidebar": "false",
+    "relbarbgcolor": "#16C",
+    "relbartextcolor": "#FEA",
+    "sidebarbgcolor": "#F2F2F2",
+    "sidebarbtncolor": "#16C",
+    "sidebartextcolor": "#16C",
+    "sidebarlinkcolor": "#16C",
+    "footerbgcolor": "#EEE",
+    "footertextcolor": "#000",
+}
+
+"""
+footerbgcolor (CSS color): Background color for the footer line.
+footertextcolor (CSS color): Text color for the footer line.
+sidebarbgcolor (CSS color): Background color for the sidebar.
+sidebarbtncolor (CSS color): Background color for the sidebar collapse button (used when collapsiblesidebar is true).
+sidebartextcolor (CSS color): Text color for the sidebar.
+sidebarlinkcolor (CSS color): Link color for the sidebar.
+relbarbgcolor (CSS color): Background color for the relation bar.
+relbartextcolor (CSS color): Text color for the relation bar.
+relbarlinkcolor (CSS color): Link color for the relation bar.
+bgcolor (CSS color): Body background color.
+textcolor (CSS color): Body text color.
+linkcolor (CSS color): Body link color.
+visitedlinkcolor (CSS color): Body color for visited links.
+headbgcolor (CSS color): Background color for headings.
+headtextcolor (CSS color): Text color for headings.
+headlinkcolor (CSS color): Link color for headings.
+codebgcolor (CSS color): Background color for code blocks.
+codetextcolor (CSS color): Default text color for code blocks, if not set differently by the highlighting style.
+bodyfont (CSS font-family): Font for normal text.
+headfont
+"""
