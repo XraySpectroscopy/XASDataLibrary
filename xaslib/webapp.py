ce06cb4b3cd23a5518baa1cd2caf6a5d0b0e4f99
Matthew Newville <newville@cars.uchicago.edu>
2020-05-17 13:46:38 -0500

    WIP fixes for users adding spectra to suites

--- xaslib/webapp.py
+++ xaslib/webapp.py
@@ -895,21 +895,26 @@ def showsuite_rating(stid=None):
 @app.route('/add_spectrum_to_suite',  methods=['GET', 'POST'])
 def add_spectrum_to_suite():
     session_init(session)
-    error=None
-    print(" Add spectrum to suite ", list(request.form.items()))
+    error = None
     if session['username'] is None:
         error='must be logged in to add spectrum to suite'
         return redirect(url_for('spectrum', spid=spid, error=error))
 
     ###
-    person_id = int(request.form.get('person', session.person_id))
-    spectrum_id = int(request.form.get('spectrum', -1))
-    target_suite = int(request.form.get('target_suite', -1))
-    print(" Add spect to suite person, suite, spectrum = ", person_id, target_suite, spectrum_id)
-    flash(add_spectra_to_suite(db, [spectrum_id],
-                               suite_id=target_suite,
-                               person_id=person_id))
-
+    person_id = int(request.form.get('person', -1))
+    if person_id < 1 and ('person_id' in request.form):
+        person_id = request.form.get('person', -1)
+    if person_id < 1:
+        session_init(session)
+        person_id = getattr(session, 'person_id', -1)
+    if person_id > 0:
+        spectrum_id = int(request.form.get('spectrum', -1))
+        target_suite = int(request.form.get('target_suite', -1))
+        msg = add_spectra_to_suite(db, [spectrum_id],
+                                   suite_id=target_suite,
+                                   person_id=person_id)
+        flash(msg)
+        return redirect(url_for('spectrum', spid=spid, error=error))
 
 @app.route('/submit_spectrum_to_suite', methods=['GET', 'POST'])
 def submit_spectrum_to_suite():
